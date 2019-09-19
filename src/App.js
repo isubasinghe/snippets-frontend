37dfd79ec0d4698e561e6dc1cb23964114ab6ad5
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2019-09-19 20:18:45 +1000

    Added initial files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AceEditor from "react-ace";
+import './App.scss';
+
+import "brace/mode/javascript";
+import "brace/theme/monokai";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="bar">
+        Snippets
+      </h1>
+      <AceEditor
+        className="editor"
+        mode="javascript"
+        theme="monokai"
+        fontSize={14}
+        showPrintMargin={true}
+        showGutter={true}
+        highlightActiveLine={true}
+        width={'100%'}
+        height={'100%'}
+        setOptions={{
+        enableBasicAutocompletion: true,
+        enableLiveAutocompletion: false,
+        enableSnippets: false,
+        showLineNumbers: true,
+        tabSize: 2,
+        }}/>
+      <div className='content-container'>
+
+      </div>
     </div>
   );
 }
