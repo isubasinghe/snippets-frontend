965e9683c3f06568309076a3a6b8d0289d920317
Isitha Subasinghe <isubasinghe@pop-os.localdomain>
2019-09-20 13:40:39 +1000

    Added completed repo

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# What is this project
+
+Its an insecure FaaS essentially, you can host your random snippets of code on here.  
+Just make sure that it isnt security critical code.  
+I have some security going on however, I have only allowed the http and net modules on here. Please let me know if other modules are required, I can write mock interfaces for fs if needed.
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,38 +1,89 @@
-import React, { useState, useEffect } from 'react';
-import { js as beautify } from 'js-beautify';
+import React, { useState, useEffect } from "react";
+import { js as beautify } from "js-beautify";
 import AceEditor from "react-ace";
-import { sha256 } from 'js-sha256';
-import './App.scss';
+import { sha256 } from "js-sha256";
+import axios from "axios";
+import "./App.scss";
 
 import "brace/mode/javascript";
 import "brace/theme/monokai";
-
+import "brace/mode/json";
+import "brace/theme/github";
 
 function App() {
-  const [source, setSource] = useState('');
-  const [hash, setHash] = useState('');
-  const [bsource, setBSource] = useState('');
+  const [source, setSource] = useState(`
+    function handler(req, cb) {
+      console.log("Started");
+      cb({data: "Hello World"});
+    }
+    
+    
+    module.exports = handler;
+  `);
+  const [hash, setHash] = useState("");
+  const [bsource, setBSource] = useState("");
+  const [postData, setPostData] = useState("");
 
   useEffect(() => {
     setBSource(beautify(source, { preserve_newlines: false }));
-  }, [source])
+  }, [source]);
 
   useEffect(() => {
     console.log(bsource);
     setHash(sha256(bsource));
   }, [bsource]);
+
   const loaded = bsource.length > 0;
 
-  
-  const deploy = (source) => {
+  const deploy = source => {
+    axios
+      .post("http://localhost:3000/api/create", {
+        source
+      })
+      .then(res => {
+        alert(res);
+        console.log(res);
+      })
+      .catch(err => {
+        alert(err);
+        console.log(err);
+      });
+  };
 
-  }
+  const post = () => {
+    let data;
+    try {
+      data = JSON.parse(postData);
+    } catch (err) {
+      alert(err.message);
+      return;
+    }
+    axios
+      .post(`http://localhost:3000/${hash}`, data)
+      .then(res => {
+        console.log(res);
+        alert(JSON.stringify(res.data));
+      })
+      .catch(err => {
+        alert(err.message);
+      });
+  };
+
+  const get = () => {
+    axios
+      .get(`http://localhost:3000/${hash}`)
+      .then(res => {
+        console.log(res);
+        alert(JSON.stringify(res.data));
+      })
+      .catch(err => {
+        alert(err.message);
+      });
+  };
 
   return (
     <div className="App">
-      <h1 className="bar">
-        Snippets
-      </h1>
+      <h1 className="bar">Snippets</h1>
       <AceEditor
         value={source}
         className="editor"
@@ -42,45 +93,72 @@ function App() {
         showPrintMargin={true}
         showGutter={true}
         highlightActiveLine={true}
-        width={'100%'}
-        height={'100%'}
-        onChange={source => {setSource(source)}}
+        width={"100%"}
+        height={"100%"}
+        onChange={source => {
+          setSource(source);
+        }}
         setOptions={{
-        enableBasicAutocompletion: true,
-        enableLiveAutocompletion: false,
-        enableSnippets: false,
-        showLineNumbers: true,
-        tabSize: 2,
-        }}/>
-      <div className='content-container'>
+          enableBasicAutocompletion: true,
+          enableLiveAutocompletion: false,
+          enableSnippets: false,
+          showLineNumbers: true,
+          tabSize: 2
+        }}
+      />
+      <div className="content-container">
         <div className="center-h">
           <div className="form-container">
             <div className="checksum-section">
-              {loaded? (
-                  <div>
-                    SHA256 CheckSum: {hash}
-                  </div>
-                ) : (
-                  <div>
-                    Enter some code first
-                  </div>
-                )}
+              {loaded ? (
+                <div>SHA256 CheckSum: {hash}</div>
+              ) : (
+                <div>Enter some code first</div>
+              )}
             </div>
             <div className="deploy-section">
-            {loaded? (
-                  <div>
-                    <div>
-                      https://api.snippets.io/{hash}/
-                    </div>
-                    <button className="button" onClick={deploy(bsource)}>
-                      Click to deploy to this endpoint
-                    </button>
-                  </div>
-                ) : (
-                  <div>
-                    Not ready to deploy
-                  </div>
-                )}
+              {loaded ? (
+                <div>
+                  <div>https://api.snippets.engineer/{hash}/</div>
+                  <button
+                    className="button"
+                    onClick={() => {
+                      deploy(source);
+                    }}
+                  >
+                    Click to deploy to this endpoint
+                  </button>
+                </div>
+              ) : (
+                <div>Not ready to deploy</div>
+              )}
+            </div>
+            <AceEditor
+              placeholder="Enter JSON Payload for POST request here"
+              value={postData}
+              className="json-editor"
+              mode="json"
+              theme="monokai"
+              fontSize={14}
+              showPrintMargin={true}
+              showGutter={true}
+              highlightActiveLine={true}
+              onChange={data => setPostData(data)}
+              setOptions={{
+                enableBasicAutocompletion: true,
+                enableLiveAutocompletion: false,
+                enableSnippets: false,
+                showLineNumbers: true,
+                tabSize: 2
+              }}
+            />
+            <div className="button-group">
+              <button className="button" onClick={post}>
+                Test POST
+              </button>
+              <button className="button" onClick={get}>
+                Test GET
+              </button>
             </div>
           </div>
         </div>
